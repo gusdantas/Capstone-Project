21e3412e7b7291872f0e5f60b0bfc859f53227fd
GUSTAVO HIDALGO/LGESP RGustavo.hidalgoD MC SW(gustavo.hidalgo@lge.com) <gustavo.hidalgo@lge.com>
2018-02-19 17:15:41 -0300

    Implement Content Provider
    Signed-off-by: GUSTAVO HIDALGO/LGESP RGustavo.hidalgoD MC SW(gustavo.hidalgo@lge.com) <gustavo.hidalgo@lge.com>

--- Quaiscalingudum/app/src/main/java/com/gustavohidalgo/quaiscalingudum/adapters/SearchLineAdapter.java
+++ Quaiscalingudum/app/src/main/java/com/gustavohidalgo/quaiscalingudum/adapters/SearchLineAdapter.java
@@ -1,6 +1,8 @@
 package com.gustavohidalgo.quaiscalingudum.adapters;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.gustavohidalgo.quaiscalingudum.R;
+import com.gustavohidalgo.quaiscalingudum.data.TripsContract;
 import com.gustavohidalgo.quaiscalingudum.interfaces.OnChooseLineListener;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,68 +21,85 @@ import butterknife.ButterKnife;
  */
 
 public class SearchLineAdapter extends RecyclerView.Adapter<SearchLineAdapter.LineViewHolder>  {
-    private ArrayList<String> mLinesFiltered, mLines;
-    private static String[] mLineChosen;
+    private final Context mContext;
+    private Cursor mCursor;
+    //private ArrayList<String> mLinesFiltered, mLines;
+    //private static String[] mLineChosen;
+    private final OnChooseLineListener mChooseLineListener;
 
-    private static OnChooseLineListener mChooseLineListener;
+    public SearchLineAdapter(Context context, OnChooseLineListener chooseLineListener){
 
-    public SearchLineAdapter(){
-
+        this.mContext = context;
+        this.mChooseLineListener = chooseLineListener;
     }
 
     @Override
     public LineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        //Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
         View view = layoutInflater.inflate(R.layout.line_item, parent, false);
+        view.setFocusable(true);
         return new LineViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(LineViewHolder holder, int position) {
-        mLineChosen = mLinesFiltered.get(position).split(",");
-        holder.mLineCodeTV.setText(mLineChosen[0]);
-        holder.mLineNameTV.setText(mLineChosen[3]);
+        mCursor.moveToPosition(position);
+
+        String code = mCursor.getString(mCursor.getColumnIndex(TripsContract.TripsEntry.ROUTE_ID));
+        String name = mCursor.getString(mCursor.getColumnIndex(TripsContract.TripsEntry.TRIP_HEADSIGN));
+        holder.mLineCodeTV.setText(code);
+        holder.mLineNameTV.setText(name);
+//        mLineChosen = mLinesFiltered.get(position).split(",");
+//        holder.mLineCodeTV.setText(mLineChosen[0]);
+//        holder.mLineNameTV.setText(mLineChosen[3]);
     }
 
     @Override
     public int getItemCount() {
-        if (mLinesFiltered != null){
-            return mLinesFiltered.size();
-        }
-        return 0;
+//        if (mLinesFiltered != null){
+//            return mLinesFiltered.size();
+//        }
+//        return 0;
+        if (null == mCursor) return 0;
+        return mCursor.getCount();
     }
 
-    public void setLines(ArrayList<String> lines){
-        this.mLines = new ArrayList<>();
-        this.mLinesFiltered = new ArrayList<>();
-        this.mLines.addAll(lines);
-        this.mLinesFiltered.addAll(lines);
+    public void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
         notifyDataSetChanged();
     }
 
-    public void setChooseLineListener(OnChooseLineListener chooseLineListener){
-        mChooseLineListener = chooseLineListener;
-    }
+//    public void setLines(ArrayList<String> lines){
+//        this.mLines = new ArrayList<>();
+//        this.mLinesFiltered = new ArrayList<>();
+//        this.mLines.addAll(lines);
+//        this.mLinesFiltered.addAll(lines);
+//        notifyDataSetChanged();
+//    }
+
+//    public void setChooseLineListener(OnChooseLineListener chooseLineListener){
+//        mChooseLineListener = chooseLineListener;
+//    }
 
     public void linesFilter(String filter) {
-        mLinesFiltered.clear();
-        if(filter.isEmpty()){
-            mLinesFiltered.addAll(mLines);
-        } else {
-            filter = filter.toLowerCase();
-            for(String item : mLines){
-                String[] line = item.split(",");
-                if(line[0].toLowerCase().replaceAll("\"", "").contains(filter)
-                        || line[3].toLowerCase().replaceAll("\"", "").contains(filter)){
-                    mLinesFiltered.add(item);
-                    notifyDataSetChanged();
-                }
-            }
-        }
+//        mLinesFiltered.clear();
+//        if(filter.isEmpty()){
+//            mLinesFiltered.addAll(mLines);
+//        } else {
+//            filter = filter.toLowerCase();
+//            for(String item : mLines){
+//                String[] line = item.split(",");
+//                if(line[0].toLowerCase().replaceAll("\"", "").contains(filter)
+//                        || line[3].toLowerCase().replaceAll("\"", "").contains(filter)){
+//                    mLinesFiltered.add(item);
+//                    notifyDataSetChanged();
+//                }
+//            }
+//        }
     }
 
-    public static class LineViewHolder extends RecyclerView.ViewHolder
+    public class LineViewHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener{
         @BindView(R.id.line_code_tv)
         TextView mLineCodeTV;
@@ -97,7 +115,16 @@ public class SearchLineAdapter extends RecyclerView.Adapter<SearchLineAdapter.Li
 
         @Override
         public void onClick(View v) {
-            mChooseLineListener.lineChosen(mLineChosen);
+            int adapterPosition = getAdapterPosition();
+//          COMPLETED (37) Instead of passing the String for the clicked item, pass the date from the cursor
+            mCursor.moveToPosition(adapterPosition);
+            //mClickHandler.onClick(dateInMillis);
+            //mChooseLineListener.lineChosen(mLineChosen);
+            String[] lineChosen = new String[mCursor.getCount()];
+            for(int i = 0; i < 6; i++){
+                lineChosen[i] = mCursor.getString(i);
+            }
+            mChooseLineListener.lineChosen(lineChosen);
         }
     }
 }
--- Quaiscalingudum/app/src/main/java/com/gustavohidalgo/quaiscalingudum/views/PickLineFragment.java
+++ Quaiscalingudum/app/src/main/java/com/gustavohidalgo/quaiscalingudum/views/PickLineFragment.java
@@ -1,8 +1,14 @@
 package com.gustavohidalgo.quaiscalingudum.views;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -14,6 +20,8 @@ import android.widget.Toast;
 
 import com.gustavohidalgo.quaiscalingudum.R;
 import com.gustavohidalgo.quaiscalingudum.adapters.SearchLineAdapter;
+import com.gustavohidalgo.quaiscalingudum.data.TripsContract;
+import com.gustavohidalgo.quaiscalingudum.data.TripsDbHelper;
 import com.gustavohidalgo.quaiscalingudum.interfaces.OnChooseLineListener;
 import com.gustavohidalgo.quaiscalingudum.interfaces.OnEditNotificationListener;
 import com.gustavohidalgo.quaiscalingudum.models.Notification;
@@ -24,6 +32,13 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.DIRECTION_ID;
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.ROUTE_ID;
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.SERVICE_ID;
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.SHAPE_ID;
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.TRIP_HEADSIGN;
+import static com.gustavohidalgo.quaiscalingudum.data.TripsContract.TripsEntry.TRIP_ID;
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -33,9 +48,22 @@ import butterknife.OnClick;
  * create an instance of this fragment.
  */
 public class PickLineFragment extends Fragment implements SearchView.OnQueryTextListener,
-        OnChooseLineListener {
+        OnChooseLineListener, LoaderManager.LoaderCallbacks<Cursor> {
     private static final String NOTIFICATION = "notification";
     private static final String LINES = "lines";
+
+    public static final String[] TRIPS_PROJECTION = {
+            ROUTE_ID, SERVICE_ID, TRIP_ID, TRIP_HEADSIGN, DIRECTION_ID, SHAPE_ID
+    };
+
+    public static final int INDEX_TRIP_ROUTE_ID = 0;
+    public static final int INDEX_TRIP_SERVICE_ID = 1;
+    public static final int INDEX_TRIP_TRIP_ID = 2;
+    public static final int INDEX_TRIP_TRIP_HEADSIGN_ID = 3;
+    public static final int INDEX_TRIP_DIRECTION_ID = 4;
+    public static final int INDEX_TRIP_SHAPE_ID = 5;
+
+    private static final int ID_TRIPS_LOADER = 353;
 
     SearchView mSearchView;
     @BindView(R.id.lines_rv)
@@ -48,6 +76,9 @@ public class PickLineFragment extends Fragment implements SearchView.OnQueryText
     private Notification mNotification;
     private ArrayList<String> mLines;
     private SearchLineAdapter mSearchLineAdapter;
+    private SQLiteDatabase mDb;
+    private Uri mUri;
+    private int mPosition = RecyclerView.NO_POSITION;
 
     private OnEditNotificationListener mListener;
 
@@ -78,6 +109,10 @@ public class PickLineFragment extends Fragment implements SearchView.OnQueryText
             mNotification = getArguments().getParcelable(NOTIFICATION);
             mLines = getArguments().getStringArrayList(LINES);
         }
+        //mUri = getActivity().getIntent().getData();
+//      COMPLETED (17) Throw a NullPointerException if that URI is null
+        //if (mUri == null) throw new NullPointerException("URI for DetailActivity cannot be null");
+        getLoaderManager().initLoader(ID_TRIPS_LOADER, null, this);
         daysFilter(mNotification.getServiceIds());
         Toast.makeText(getActivity(), "stop", Toast.LENGTH_SHORT).show();
     }
@@ -90,10 +125,12 @@ public class PickLineFragment extends Fragment implements SearchView.OnQueryText
         ButterKnife.bind(this, view);
         mSearchView = view.findViewById(R.id.searchView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        TripsDbHelper tripsDbHelper = new TripsDbHelper(getActivity());
+        mDb = tripsDbHelper.getWritableDatabase();
         mLinesRV.setLayoutManager(layoutManager);
-        mSearchLineAdapter = new SearchLineAdapter();
-        mSearchLineAdapter.setChooseLineListener(this);
-        mSearchLineAdapter.setLines(mLines);
+        mSearchLineAdapter = new SearchLineAdapter(getActivity(), this);
+        //mSearchLineAdapter.setChooseLineListener(this);
+        //mSearchLineAdapter.setLines(mLines);
         mLinesRV.setAdapter(mSearchLineAdapter);
         mSearchView.setOnQueryTextListener(this);
         return view;
@@ -171,4 +208,48 @@ public class PickLineFragment extends Fragment implements SearchView.OnQueryText
             mListener.toEta(mNotification);
         }
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+
+            case ID_TRIPS_LOADER:
+                /* URI for all rows of weather data in our weather table */
+                Uri tripsQueryUri = TripsContract.TripsEntry.CONTENT_URI;
+                /* Sort order: Ascending by date */
+                String sortOrder = TripsContract.TripsEntry.TRIP_ID + " ASC";
+                /*
+                 * A SELECTION in SQL declares which rows you'd like to return. In our case, we
+                 * want all weather data from today onwards that is stored in our weather table.
+                 * We created a handy method to do that in our WeatherEntry class.
+                 */
+                //String selection = WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards();
+
+                return new CursorLoader(getActivity(),
+                        tripsQueryUri,
+                        TRIPS_PROJECTION,
+                        null,
+                        null,
+                        sortOrder);
+
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mSearchLineAdapter.swapCursor(data);
+        if (mPosition == RecyclerView.NO_POSITION) mPosition = 0;
+        mLinesRV.smoothScrollToPosition(mPosition);
+        //if (data.getCount() != 0) showWeatherDataView();
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mSearchLineAdapter.swapCursor(null);
+
+    }
+
 }
